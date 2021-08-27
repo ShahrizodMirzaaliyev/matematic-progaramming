08a20c6a98b2697dc9069881eb205b14c4e3d2c2
ShahrizodMirzaaliyev <88931792+ShahrizodMirzaaliyev@users.noreply.github.com>
2021-08-27 17:57:09 +0300

    yangi tk.py
    yangi fayl

---
+++ tk.py
@@ -0,0 +1,48 @@
+import random
+import math
+import datetime
+
+
+
+def son_kopaytir(x):
+    """" bu funksiyani chaqirgandan song uning orqasidan biror bir o'zgaruvchi berish kerak"""""
+    info = f"{x*2}"
+    return info
+
+
+def son_kvadrati(x):
+    """" bu funksiya sonning  kvadratini aniqlaydi bu funksiyani chaqirgandan so'ng uning orqasidan biror bir o'zgaruvchi kiritish lozim"""""
+    info = f'Bu sonning kvadradi {x**2} ga teng'
+    return info,
+
+
+def sonning_cosini_bil(x):
+    """ bu funksiya sonning kosinusini bilib beradi
+    """
+
+    info =  f"{math.cos(x)}"
+    return info
+
+
+
+def sonni_sin_bil(x):
+    """" bu funksya sonning sin ini bilishda yordam beradi, funksiyani chaqirgandan so'ng orqasidan o'zgartiruvchi kiriting"""""
+    info = f'{math.sin(x)}'
+    return info
+
+
+def sonni_pow_bil(x,y):
+    """" bu funksiya darajasini bilishda yordam beradi"""""
+    info = f'{math.pow(x,y)}'
+    return info
+
+
+
+
+
+
+
+
+
+
+
